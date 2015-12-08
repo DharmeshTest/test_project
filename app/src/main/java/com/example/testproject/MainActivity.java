53f202a83a0e8fb9e740648a60b903107cc22116
Dharmesh <dh96663@gmail.com>
2015-12-08 16:04:04 +0530

    test commit 4

--- app/src/main/java/com/example/testproject/MainActivity.java
+++ app/src/main/java/com/example/testproject/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.i(TAG_TEST, "Test 3");
+        Log.i(TAG_TEST, "Test 4");
     }
 
     @Override
